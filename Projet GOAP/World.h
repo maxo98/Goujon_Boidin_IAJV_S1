6c4bbbb91c8755c12dd9e4f6cf2f12a303b96d29
Mute <40270424+Ced0@users.noreply.github.com>
2022-09-13 18:35:25 +0200

    Finition GOAP
    image, readMe, world.h

--- Projet GOAP/World.h
+++ Projet GOAP/World.h
@@ -1,8 +1,8 @@
 #pragma once
 
+#define WORLD_1
 
-//World 1
-
+#ifdef WORLD_1
 #define COST_STEAL 15
 #define AMMO 0
 #define WEAPON 1
@@ -11,63 +11,70 @@
 #define DEGREE false
 #define LOW_HEALTH true
 #define HEALTH_POTION 0
+#endif // WORLD 1
+
+
 
 
 //World 2
-
-//#define COST_STEAL 3
-//#define AMMO 0
-//#define WEAPON 1
-//#define PLAYER_DISTANCE 5
-//#define MONEY false
-//#define DEGREE false
-//#define LOW_HEALTH true
-//#define HEALTH_POTION 0
-
+#ifdef WORLD_2
+#define cost_steal 3
+#define ammo 0
+#define weapon 1
+#define player_distance 5
+#define money false
+#define degree false
+#define low_health true
+#define health_potion 0
+#endif // WORLD 2
 
 
 ////World 3
-//
-//#define COST_STEAL 15
-//#define AMMO 0
-//#define WEAPON 0
-//#define PLAYER_DISTANCE 5
-//#define MONEY false
-//#define DEGREE false
-//#define LOW_HEALTH true
-//#define HEALTH_POTION 1
-
+#ifdef WORLD_3
+#define COST_STEAL 15
+#define AMMO 0
+#define WEAPON 0
+#define PLAYER_DISTANCE 5
+#define MONEY false
+#define DEGREE false
+#define LOW_HEALTH true
+#define HEALTH_POTION 1
+#endif // WORLD 3
 
 
 //World 4
-
-//#define COST_STEAL 15
-//#define AMMO 0
-//#define WEAPON 0
-//#define PLAYER_DISTANCE 5
-//#define MONEY true
-//#define DEGREE false
-//#define LOW_HEALTH true
-//#define HEALTH_POTION 0
+#ifdef WORLD_4
+#define COST_STEAL 15
+#define AMMO 0
+#define WEAPON 0
+#define PLAYER_DISTANCE 5
+#define MONEY true
+#define DEGREE false
+#define LOW_HEALTH true
+#define HEALTH_POTION 0
+#endif // WORLD 4
 
 //World 5
+#ifdef WORLD_5
+#define COST_STEAL 15
+#define AMMO 5
+#define WEAPON 1
+#define PLAYER_DISTANCE 5
+#define MONEY false
+#define DEGREE true
+#define LOW_HEALTH true
+#define HEALTH_POTION 0
+#endif // WORLD 5
 
-//#define COST_STEAL 15
-//#define AMMO 5
-//#define WEAPON 1
-//#define PLAYER_DISTANCE 5
-//#define MONEY false
-//#define DEGREE true
-//#define LOW_HEALTH true
-//#define HEALTH_POTION 0
 
 //World 6
-
-//#define COST_STEAL 15
-//#define AMMO 5
-//#define WEAPON 1
-//#define PLAYER_DISTANCE 5
-//#define MONEY false
-//#define DEGREE false
-//#define LOW_HEALTH false
-//#define HEALTH_POTION 0
+#ifdef WORLD_6
+#define COST_STEAL 15
+#define AMMO 5
+#define WEAPON 1
+#define PLAYER_DISTANCE 5
+#define MONEY false
+#define DEGREE false
+#define LOW_HEALTH false
+#define HEALTH_POTION 0
+#endif // WORLD 5
